fd107d7605bc42ca0ee0b77c70f1e09bd7d2706e
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-08-29 11:17:43 +0300

    Changed assignment

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('mom'));
+  console.log('');
+
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('abba'));
+  console.log('');
+
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('a'));
+  console.log('');
+
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('hi'));
+  console.log('');
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
